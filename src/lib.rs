b1d99459d1f53a6026b1d07bf4e4d676bfd3e787
Charith Ellawala <charith@cerbos.dev>
2022-04-25 10:40:01 +0100

    Tweak workflow
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,52 @@
 // Copyright 2021-2022 Zenauth Ltd.
 // SPDX-License-Identifier: Apache-2.0
+//
+//! Client library for communicating with a [Cerbos](https://cerbos.dev) policy decision point (PDP). Cerbos is the open core, language-agnostic, scalable
+//! authorization solution that makes user permissions and authorization simple to implement
+//! and manage by writing context-aware access control policies for your application resources.
+//! See https://docs.cerbos.dev for more information about Cerbos.
+//!
+//! ## Example
+//!
+//! ```rust,no_run
+//! use cerbos::sdk::attr::attr;
+//! use cerbos::sdk::model::{Principal, Resource};
+//! use cerbos::sdk::{CerbosAsyncClient, CerbosClientOptions, CerbosEndpoint, Result};
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<()> {
+//!     let opt = CerbosClientOptions::new(CerbosEndpoint::HostPort("localhost", 3593));
+//!     let mut client = CerbosAsyncClient::new(opt).await?;
+//!
+//!     let principal = Principal::new("alice", ["employee"])
+//!         .with_policy_version("20210210")
+//!         .with_attributes([
+//!             attr("department", "marketing"),
+//!             attr("geography", "GB"),
+//!             attr("team", "design"),
+//!         ]);
+//!
+//!     let resource = Resource::new("XX125", "leave_request")
+//!         .with_policy_version("20210210")
+//!         .with_attributes([
+//!             attr("department", "marketing"),
+//!             attr("geography", "GB"),
+//!             attr("team", "design"),
+//!             attr("owner", "alice"),
+//!             attr("approved", true),
+//!             attr("id", "XX125"),
+//!         ]);
+//!
+//!     let resp = client
+//!         .is_allowed("view:public", principal, resource, None)
+//!         .await?;
+//!
+//!     println!("Allowed={:?}", resp);
+//!
+//!     Ok(())
+//! }
+//!
+//! ```
 
 #[path = "genpb"]
 pub mod genpb {
