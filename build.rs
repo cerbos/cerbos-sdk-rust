7c659e20329dc6f26d88caee2e900b7e3d016741
Charith Ellawala <charith@cerbos.dev>
2024-09-30 07:23:59 +0100

    Fix build script
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ fn main() -> Result<(), std::io::Error> {
     tonic_build::configure()
         .out_dir("src/genpb")
         .build_server(false)
-        .compile(&["proto/defs/cerbos/svc/v1/svc.proto"], &["proto/defs/"])
+        .compile_protos(&["proto/defs/cerbos/svc/v1/svc.proto"], &["proto/defs/"])
 }
