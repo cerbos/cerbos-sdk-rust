647805198a2bfed591814eaa36614f2ca0c1392b
Dennis Buduev <dbuduev@users.noreply.github.com>
2024-10-14 20:05:35 +1100

    fix build.rs
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ fn main() -> Result<(), std::io::Error> {
     tonic_build::configure()
         .out_dir("src/genpb")
         .build_server(false)
-        .compile_protos(&["proto/defs/cerbos/svc/v1/svc.proto"], &["proto/defs/"])
+        .compile(&["proto/defs/cerbos/svc/v1/svc.proto"], &["proto/defs/"])
 }
