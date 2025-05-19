c385af1bf9af68fabc38c56188040796dc742e90
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-05-19 19:06:48 +1000

    cloud-api
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- proto/download-protos.sh
+++ proto/download-protos.sh
@@ -7,9 +7,11 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROTO_DIR="${SCRIPT_DIR}/defs"
 CERBOS_MODULE=${CERBOS_MODULE:-"buf.build/cerbos/cerbos-api"}
+CLOUD_MODULE=${CLOUD_MODULE:-"buf.build/cerbos/cloud-api"}
 
 rm -rf "$PROTO_DIR"
 (
     cd "$SCRIPT_DIR"
     buf export "$CERBOS_MODULE" --output="${PROTO_DIR}"
+    buf export "$CLOUD_MODULE" --output="${PROTO_DIR}" --path cerbos/cloud/store/v1
 )
